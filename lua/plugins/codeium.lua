732442cbd49a5d3471e0306f10aa376afb28909e
Terry <tsdevendra1@gmail.com>
2025-03-13 14:24:42 +0000

    Minor changes

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,12 +1,29 @@
 return {
-  "hrsh7th/nvim-cmp",
-  optional = true,
-  dependencies = { "codeium.nvim" },
-  opts = function(_, opts)
-    table.insert(opts.sources, 1, {
-      name = "codeium",
-      group_index = 1,
-      priority = 100,
-    })
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "codeium.nvim" },
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+        enable_cmp_source = true,
+        virtual_text = {
+          enabled = false,
+        },
+      })
+    end,
+  },
 }
