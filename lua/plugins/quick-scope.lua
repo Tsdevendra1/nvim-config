2a80a0193b8185f688418a7be67e439cd85ebe5d
Terry <tsdevendra1@gmail.com>
2025-03-16 20:32:44 +0000

    Works for vscode now

--- lua/plugins/quick-scope.lua
+++ lua/plugins/quick-scope.lua
@@ -1,14 +1,4 @@
 return {
   "unblevable/quick-scope",
-  lazy = false,
-  config = function()
-    -- Set highlight colors that complement Tokyo Night theme using proper autocmd method
-    vim.cmd([[
-      augroup qs_colors
-        autocmd!
-        autocmd ColorScheme * highlight QuickScopePrimary guifg='#ff9e64' gui=underline ctermfg=155 cterm=underline
-        autocmd ColorScheme * highlight QuickScopeSecondary guifg='#7dcfff' gui=underline ctermfg=81 cterm=underline
-      augroup END
-    ]])
-  end,
+  vscode = true, -- Add this line
 }
